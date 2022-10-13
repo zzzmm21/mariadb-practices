b1bf0026d5abe97ed323387a05510ad43d74c4c0
zzzmm21 <114974461+zzzmm21@users.noreply.github.com>
2022-10-13 17:46:07 +0900

    Add files via upload

--- mariadb-practies/sql 연습문제/연습문제 2.sql
+++ mariadb-practies/sql 연습문제/연습문제 2.sql
@@ -11,21 +11,30 @@ from salaries;
 -- 문제2.
 -- 마지막으로 신입사원이 들어온 날은 언제 입니까? 다음 형식으로 출력해주세요.
 -- 예) 2014년 07월 10일
-select 
-from employees\
-where 
+select date_format(max(hire_date), '%Y년 %m월 %d일') as '마지막 신입사원'
+from employees;
+
 
 
 
 -- 문제3.
 -- 가장 오래 근속한 직원의 입사일은 언제인가요? 다음 형식으로 출력해주세요.
 -- 예) 2014년 07월 10일
+select date_format(min(hire_date), '%Y년 %m월 %d일') as '오래된 직원'
+from employees;
 
 -- 문제4.
 -- 현재 이 회사의 평균 연봉은 얼마입니까?
+select avg(salary) as 평균연봉
+from salaries
+where to_date = '9999-01-01';
 
 -- 문제5.
 -- 현재 이 회사의 최고/최저 연봉은 얼마입니까?
+select max(salary)as '최고연봉'  , min(salary) as '최저 연봉'
+from salaries;
 
--- 문제6.
--- 최고 어린 사원의 나이와 최 연장자의 나이는?
+-- 문제6 max(birth_date) as '어린사원 ', min(birth_date) as '최연장 사원'.
+-- 최고 어린 사원의 나이와 최 연장자의 나이는? 
+select  *
+f
